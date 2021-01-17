f5109f00895675826cc2cb144742463a6f290211
赵苗苗 <>
2021-01-17 18:50:21 +0800

    ‘公众属性’

---
+++ ZMMAnalyticsSDK/BuinessModules/Properties/OutgoingService/ZMMAPropertiesManagerInterface.h
@@ -0,0 +1,16 @@
+//
+//  ZMMAPropertiesManagerInterface.h
+//  ZMMAnalyticsSDK
+//
+//  Created by 赵苗苗 on 2021/1/17.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@protocol ZMMAPropertiesManagerInterface <NSObject>
+
+@end
+
+NS_ASSUME_NONNULL_END
