8bae0fb0d1a38d2c7b2492e2309312dbb39bc5fe
赵苗苗 <>
2021-01-17 11:46:27 +0800

    提交外部调用的类

--- ZMMAnalyticsSDK/ZMMAnalyticsSDK.h
+++ ZMMAnalyticsSDK/ZMMAnalyticsSDK.h
@@ -7,12 +7,15 @@
 
 #import <Foundation/Foundation.h>
 
-//! Project version number for ZMMAnalyticsSDK.
-FOUNDATION_EXPORT double ZMMAnalyticsSDKVersionNumber;
+NS_ASSUME_NONNULL_BEGIN
 
-//! Project version string for ZMMAnalyticsSDK.
-FOUNDATION_EXPORT const unsigned char ZMMAnalyticsSDKVersionString[];
+@interface ZMMAnalyticsSDK : NSObject
 
-// In this header, you should import all the public headers of your framework using statements like #import <ZMMAnalyticsSDK/PublicHeader.h>
+#pragma mark- init instance
+
++ (ZMMAnalyticsSDK * _Nullable)sharedInstance;
 
 
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ ZMMAnalyticsSDK/ZMMAnalyticsSDK.m
@@ -0,0 +1,23 @@
+//
+//  ZMMAnalyticsSDK.m
+//  ZMMAnalyticsSDK
+//
+//  Created by 赵苗苗 on 2021/1/16.
+//
+
+#import "ZMMAnalyticsSDK.h"
+
+@interface ZMMAnalyticsSDK ()
+
+
+
+@end
+
+@implementation ZMMAnalyticsSDK
+
+static ZMMAnalyticsSDK *
+
+#pragma mark - Initialization
+
+
+@end
