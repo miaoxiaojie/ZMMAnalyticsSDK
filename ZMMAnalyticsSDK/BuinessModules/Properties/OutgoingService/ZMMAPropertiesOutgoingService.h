26ae3d3b9341ebe1831bdaa5da183a168d749d06
赵苗苗 <>
2021-01-18 15:51:45 +0800

    公关属性外部类

---
+++ ZMMAnalyticsSDK/BuinessModules/Properties/OutgoingService/ZMMAPropertiesOutgoingService.h
@@ -0,0 +1,16 @@
+//
+//  ZMMAPropertiesOutgoingService.h
+//  ZMMAnalyticsSDK
+//
+//  Created by 赵苗苗 on 2021/1/17.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ZMMAPropertiesOutgoingService : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ ZMMAnalyticsSDK/BuinessModules/Properties/OutgoingService/ZMMAPropertiesOutgoingService.m
@@ -0,0 +1,12 @@
+//
+//  ZMMAPropertiesOutgoingService.m
+//  ZMMAnalyticsSDK
+//
+//  Created by 赵苗苗 on 2021/1/17.
+//
+
+#import "ZMMAPropertiesOutgoingService.h"
+
+@implementation ZMMAPropertiesOutgoingService
+
+@end
